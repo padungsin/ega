848c02b6f7f02062e9e5946c98eec417542c7934
padungsin <padungsin@MARLO10>
2017-07-29 17:07:59 +0700

    modify

--- vagrant/teiid/teiid1/script/script.sh
+++ vagrant/teiid/teiid1/script/script.sh
@@ -21,14 +21,22 @@ if [ ! -d "/opt/teiid" ]; then
 	cp -r /vagrant_data/teiid/modules/* /opt/teiid/modules/
 	yes|cp /vagrant_data/teiid/standalone-teiid.xml /opt/teiid/standalone/configuration/
 
+	mkdir -p /var/log/teiid
 
 	echo "Store password in vault"
 	mkdir -p /opt/teiid/vault
 	keytool -genseckey -alias vault -keystore /opt/teiid/vault/vault.keystore -storetype jceks -keyalg AES -keysize 128 -storepass vault22 -keypass vault22
 
+	echo "Adding MariaDB Password"
+	sh /opt/teiid/bin/vault.sh -k /opt/teiid/vault/vault.keystore -p vault22 -e /opt/teiid/vault -i 120 -s 1234abcd -v vault -b mariadb -a password -x Password1!
+
 
 	echo "Generate SSL Key"
 	keytool -genkeypair -alias teiid -storetype jks -keyalg RSA -keysize 2048 -keypass Password1! -keystore /opt/teiid/standalone/configuration/teiid.jks -storepass Password1! -dname "CN=teiid,OU=ega,L=Bangkok,C=TH" -validity 730 -v
+
+
+	echo "Adding Keystore Password"
+	sh /opt/teiid/bin/vault.sh -k /opt/teiid/vault/vault.keystore -p vault22 -e /opt/teiid/vault -i 120 -s 1234abcd -v vault -b keystore -a password -x Password1!
 
 	keytool -importkeystore -srckeystore /opt/teiid/standalone/configuration/teiid.jks \
 	       -destkeystore /vagrant_data/ha-proxy/teiid-ssl-key/teiid.p12 \
@@ -40,8 +48,6 @@ if [ ! -d "/opt/teiid" ]; then
 	openssl pkcs12 -in /vagrant_data/ha-proxy/teiid-ssl-key/teiid.p12 -out /vagrant_data/ha-proxy/teiid-ssl-key/teiid.pem -password pass:Password1! 
 	sh /opt/teiid/bin/vault.sh --keystore /opt/teiid/vault/vault.keystore --keystore-password vault22 --alias vault --vault-block vb --attribute teiid --sec-attr password1! --enc-dir /opt/teiid/vault/ --iteration 120 --salt 1234abcd
 
-	echo "Adding MariaDB Password"
-	sh /opt/teiid/bin/vault.sh -k /opt/teiid/vault/vault.keystore -p vault22 -e /opt/teiid/vault -i 120 -s 1234abcd -v vault -b mariadb -a password -x Password1!
 
 
 	touch /opt/teiid/standalone/configuration/https-users.properties
@@ -50,6 +56,11 @@ if [ ! -d "/opt/teiid" ]; then
 	useradd -r teiid
 	chown -R teiid:teiid /opt/teiid*
 	chmod -R 755 /opt/teiid*
+
+	chown -R teiid:teiid /var/log/teiid
+	chmod -R 755 /var/log/teiid
+
+
 
 	echo "Register teiid service"
 	ln -s /opt/teiid/docs/contrib/scripts/init.d/wildfly-init-redhat.sh /etc/init.d/teiid.sh
